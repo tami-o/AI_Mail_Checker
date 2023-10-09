1c0246244763a289916fa50020292efaa246859b
tami-o <m1m23a02@oit.ac.jp>
2023-10-09 22:27:10 +0900

    [Fix] main : 画面遷移でviewを毎回新規作成しない

--- main.py
+++ main.py
@@ -1,59 +1,63 @@
+# python using-flet
 import flet as ft
-
 
 def main(page: ft.Page):
 
-    mes = "view1"
-    mes_field = ft.TextField(value=mes, width=100)
-
-    def note_mes(e):
-        message = mes_field.value
-        print(message)
-
-    def create_view1():
-        return ft.View("/view1", [
+    # 情報入力画面
+    view1: ft.View = ft.View(
+        "/view1",
+         [
             ft.AppBar(title=ft.Text("view1"),
                       bgcolor=ft.colors.BLUE),
-            mes_field,
+            ft.TextField(value="view1"),
             ft.ElevatedButton(
-                #"Go to view2", on_click=lambda _: page.go("/view2")),
-                "Go to view2", on_click=note_mes),
-        ])
+                "入力確認画面", on_click=lambda _: page.go("/view2")),
+        ]
+    )
 
-    def create_view2():
-        return ft.View("/view2", [
+    # メール確認画面git 
+    view2: ft.View = ft.View(
+        "/view2", 
+        [
             ft.AppBar(title=ft.Text("view2"),
                       bgcolor=ft.colors.RED),
-            mes_field,
+            ft.TextField(value="view2"),
             ft.ElevatedButton(
-                #"Go to view1", on_click=lambda _: page.go("/view1")),
-                "Go to view2", on_click=note_mes),
-        ])
+                "確定・送信", on_click=lambda _: page.go("/view3")),
+        ]
+    )
+
+    # 結果表示画面
+    view3: ft.View = ft.View(
+        "/view3", 
+        [
+            ft.AppBar(title=ft.Text("view3"),
+                      bgcolor=ft.colors.RED),
+            ft.TextField(value="view3"),
+            ft.ElevatedButton(
+                "最初の画面", on_click=lambda _: page.go("/view1")),
+        ]
+    )
 
     def route_change(handler):
-        troute = ft.TemplateRoute(handler.route)
-        if troute.match("/view1"):
-            page.views.append(create_view1())
-        elif troute.match("/view2"):
-            page.views.append(create_view2())
+        page.views.clear()
+        if page.route != "/view3":
+            page.views.append(view1)
+            if page.route == "/view2":
+                page.views.append(view2)
+        else:
+            pass
         page.update()
 
-    # ルート変更時のロジック設定
-    page.on_route_change = route_change
-
     def view_pop(handler):
-        page.views.pop()  # 1つ前に戻る
+        page.views.pop()
         page.go("/back")
-        # page.update()
-        # update() だと route が変更されない。
-        # そうなると1つ戻ってまた進むことができなくなるので go("/back") で回避。不具合？
 
-    # 戻る時のロジック設定
+
+    page.on_route_change = route_change
     page.on_view_pop = view_pop
 
-    # Page レイアウト
     page.title = "Navigation and routing"
-    # 初期表示
     page.views.clear()
     page.go("/view1")
 
